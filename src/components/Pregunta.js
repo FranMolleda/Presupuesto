3a005708b5d091b073934d43691a13786a50f95a
Fran <fmjerez@icloud.com>
2020-07-23 15:11:25 +0200

    Validación. mensaje de error, almacenar presupuesto hecho

--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,18 +1,35 @@
 import React, { Fragment, useState } from "react";
+import Error from "./Error";
 
-const Pregunta = () => {
+const Pregunta = ({ setPresupuesto, setRestante }) => {
   const [cantidad, setCantidad] = useState(0);
-
+  const [error, setError] = useState(false);
   const handlePresupuesto = (e) => {
-    e.preventDefault();
     //Le ponemos parseInt ya que los datos recogidos de un input son string, de esta manera los convertimos a numeros
     setCantidad(parseInt(e.target.value));
+  };
+
+  const agregarPresupuesto = (e) => {
+    e.preventDefault();
+
+    //Validar
+    if (cantidad <= 0 || isNaN(cantidad)) {
+      setCantidad(0);
+      setError(true);
+      return;
+    }
+
+    //Que hacer si pasa la validación
+    setError(false);
+    setPresupuesto(cantidad);
+    setRestante(cantidad);
   };
 
   console.log(cantidad);
   return (
     <Fragment>
       <h2>Indica tu presupuesto</h2>
+      {error ? <Error mensaje="Hubo un error en el Presupuesto" /> : null}
       <form onSubmit={agregarPresupuesto}>
         <input
           type="number"
